c38cdddce8f0c4a017892b7c98a5d09e65dd3b0a
Cody Green <cody@codygreen.com>
2019-10-23 22:54:29 -0500

    update readme and var descriptions

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# terraform-aws-agility-lab-base
+# Terraform Agility Lab Base for AWS
 F5 Agility Lab base module for AWS.  This will create the required VPC, networking and security groups.
--- variables.tf
+++ variables.tf
@@ -1,27 +1,37 @@
-variable "AccessKeyID" {}
+variable "AccessKeyID" {
+    description = "AWS Access Key ID"
+}
 
-variable "SecretAccessKey" {}
+variable "SecretAccessKey" {
+    description = "AWS Secret Access Key"
+}
 
 variable "prefix" {
   default = "tf-aws-agility-base"
+  description = "naming prefix to uniquely identify AWS VPC resource"
 }
 
 variable "region" {
   default = "us-east-2"
+  description = "AWS Region to deploy into"
 }
 
 variable "azs" {
   default = ["us-east-2a", "us-east-2b"]
+  description = "AWS Availability Zones in the choosen region"
 }
 
 variable "cidr" {
   default = "10.0.0.0/16"
+  description "VPC CIDR block"
 }
 
 variable "allowed_mgmt_cidr" {
   default = "0.0.0.0/0"
+  description = "Allowed management IP address CIDR"
 }
 
 variable "allowed_app_cidr" {
   default = "0.0.0.0/0"
+  description = "Allowed applicatiion access IP address CIDR"
 }
